834ef07af2e6e64b4f40e42002a0b8b5a9f8277d
Ruslanas <ruslanas.g20@gmail.com>
2021-06-10 13:35:12 +0300

    Update delete_personal.php

--- delete_personal.php
+++ delete_personal.php
@@ -1,7 +1,13 @@
 <?php include_once ("db.php");
-global $conn;
-$id = mysqli_real_escape_string($conn,$_GET['id']);
-$sql = "DELETE FROM registrations WHERE id='$id'";
-$conn->query($sql);
-$location = "personal.php";
-header("Location:" . $location);
+session_start();
+if(!isset($_SESSION['role'])){
+    $location = "index.php";
+    header("Location:" . $location);
+} else {
+    global $conn;
+    $id = mysqli_real_escape_string($conn,$_GET['id']);
+    $sql = "DELETE FROM registrations WHERE id='$id'";
+    $conn->query($sql);
+    $location = "personal.php";
+    header("Location:" . $location);
+}
